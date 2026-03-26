e6ec6848c38922f06e3704466eff82c84510b006
nsilvera0122 <nilsonsilvera0122@gmail.com>
2026-03-26 17:59:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarMensaje() {
+    alert("La energía solar es el futuro 🌞");
+}
+
+function scrollToSection() {
+    document.getElementById("info").scrollIntoView({
+        behavior: "smooth"
+    });
+}
